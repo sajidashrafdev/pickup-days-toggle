e0cee3ae0c8e6e21dd1adf634201baaed2484395
Sajid Ashraf <sajidashraf.dev@gmail.com>
2026-04-21 23:21:43 +0500

    Add settings menu for Pickup Days

--- pickup-days-toggle.php
+++ pickup-days-toggle.php
@@ -7,3 +7,20 @@ Author: Sajid Ashraf
 Author URI: https://pk.linkedin.com/in/sajidashrafdev
 Requires Plugins: woocommerce
 */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// ===============================
+// 1. CREATE SETTINGS MENU
+// ===============================
+add_action('admin_menu', function() {
+    add_menu_page(
+        'Pickup Days Settings',
+        'Pickup Days',
+        'manage_options',
+        'pickup-days-settings',
+        'pdt_settings_page',
+        'dashicons-calendar',
+        25
+    );
+});
