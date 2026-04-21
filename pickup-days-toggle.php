7c5b13a75df5501a2da73f7662996a8afbc4cfcc
Sajid Ashraf <sajidashraf.dev@gmail.com>
2026-04-21 23:20:06 +0500

    Add plugin header for Pickup Days Toggle

---
+++ pickup-days-toggle.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Pickup Days Toggle
+Plugin URI: https://github.com/sajidashrafdev/pickup-days-toggle
+Description: Toggle pickup days from backend and control Elementor tabs visibility.
+Version: 1.1
+Author: Sajid Ashraf
+Author URI: https://pk.linkedin.com/in/sajidashrafdev
+Requires Plugins: woocommerce
+*/
